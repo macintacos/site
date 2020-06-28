084da0ba996aba26877de5e2ff99c217c68a0d38
Julian Torres <juliantorres@hey.com>
2020-06-28 01:32:30 -0400

    GH-32 ⚡️ Refactor styling of the website to use TailwindCSS, reset to "bare bones" styling (#42)

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,59 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
+
+// Plugins
+const { FontAwesomeIcon } = require("@campj/eleventy-fa-icons");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginErrorOverlay = require("eleventy-plugin-error-overlay");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const uslug = require("uslug");
 const uslugify = (s) => uslug(s);
 
 module.exports = function (eleventyConfig) {
+  // Plugins {{{
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginErrorOverlay);
+  // }}}
 
   eleventyConfig.setDataDeepMerge(true);
-
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addNunjucksShortcode("FontAwesomeIcon", FontAwesomeIcon);
 
+  // Filters {{{
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
     );
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
-  // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
+    // Get the first `n` elements of a collection.
     if (n < 0) {
       return array.slice(n);
     }
 
     return array.slice(0, n);
   });
+  // }}}
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  eleventyConfig.addCollection(
+    "tagList",
+    require("./src/site/_11ty/getTagList")
+  );
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("style");
+  eleventyConfig.addPassthroughCopy("./src/site/img");
+  eleventyConfig.addPassthroughCopy("./src/site/style/build");
 
-  /* Markdown Overrides */
+  // Markdown Overrides
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
@@ -56,12 +67,12 @@ module.exports = function (eleventyConfig) {
 
   // Browsersync Overrides
   eleventyConfig.setBrowserSyncConfig({
+    ...eleventyConfig.browserSyncConfig,
     callbacks: {
       ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
+        const content_404 = fs.readFileSync("dist/404.html");
 
         browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
         });
@@ -72,28 +83,16 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    templateFormats: ["md", "njk", "html", "liquid"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-
-    // These are all optional, defaults are shown:
     dir: {
-      input: ".",
+      input: "src/site",
       includes: "_includes",
       data: "_data",
-      output: "_site",
+      output: "dist",
     },
+    templateFormats: ["md", "njk", "html", "liquid"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
   };
 };
---
+++ src/site/about/index.md
@@ -0,0 +1,30 @@
+---
+layout: layouts/post.njk
+title: About Me
+templateClass: tmpl-post
+eleventyNavigation:
+  key: About Me
+  order: 3
+---
+
+Right now I support cloud-based databases at [MongoDB](https://www.mongodb.com/).
+
+Sometimes I write stuff or work on projects that don't go anywhere. This is where I'd post them if I'm not lazy enough to forget to do it.
+
+Here's how to reach me:
+
+{% FontAwesomeIcon
+  name = "internet-explorer",
+  type = "brand",
+  class = "text-base text-blue-400 font-awesome-brand-icon"
+%} Email: [julian.m.tor@gmail.com](mailto:julian.m.tor@gmail.com)
+{% FontAwesomeIcon
+  name = "github-alt",
+  type = "brand",
+  class = "text-base font-awesome-brand-icon"
+%} Github: [macintacos](https://github.com/macintacos)
+{% FontAwesomeIcon
+  name = "twitter",
+  type = "brand",
+  class = "text-base text-blue-400 font-awesome-brand-icon"
+%} Twitter: [@macintacos](https://twitter.com/macintacos)
