1adb95ca4ee164495761eac7fec799118d488770
Julian Torres <juliantorres@hey.com>
2020-07-06 02:13:30 -0400

    fixing baseine styles so that we're using Tailwind as much as possible (instead of defining my own styles)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: { content: ["./templates/**/*.njk"] },
   theme: {
     extend: {},
+    inset: {
+      "0": 0,
+      auto: "auto",
+      "1/2": "50%",
+    },
   },
   variants: {},
   plugins: [],
