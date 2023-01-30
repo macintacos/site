3680881f2291b7918d17dbbaab545e23560f8d1c
Julian Torres <julian.m.tor@gmail.com>
2023-01-29 20:32:23 -0500

    feat: add tailwind/lint rules for it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://excessive.dev'
+	site: 'https://excessive.dev',
+	integrations: [tailwind()]
 });
